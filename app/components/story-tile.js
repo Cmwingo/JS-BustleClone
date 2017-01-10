6e2b1441509e6b81d9c436a376664fcc46490d31
Wingo <cmwingo@gmail.com>
2017-01-10 15:41:02 -0800

    Allow admin to delete a story

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(story) {
+      if(confirm('Are you sure you would like to delete this story?')) {
+        this.sendAction('destroyStory', story);
+      }
+    }
+  }
 });
